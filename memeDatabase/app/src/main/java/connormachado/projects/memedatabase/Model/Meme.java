8644dd804d2d64c0c8267d6e5d88adf8deb18e5a
connormachado <cmachado@vassar.edu>
2022-12-16 17:43:06 -0500

    Updated Meme class

--- memeDatabase/app/src/main/java/connormachado/projects/memedatabase/Model/Meme.java
+++ memeDatabase/app/src/main/java/connormachado/projects/memedatabase/Model/Meme.java
@@ -1,4 +1,11 @@
 package connormachado.projects.memedatabase.Model;
 
 public class Meme {
+    String name;
+    String type;
+
+    public Meme(String name, String type){
+        this.name = name;
+        this.type = type;
+    }
 }
